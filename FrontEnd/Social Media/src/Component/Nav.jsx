cce628da67f31f534753dea17e7b7b604eefdf2b
HARSHIL4636 <patelharshil4636@gmail.com>
2024-12-08 21:00:44 +0530

    Your commit messag

--- FrontEnd/Social Media/src/Component/Nav.jsx
+++ FrontEnd/Social Media/src/Component/Nav.jsx
@@ -137,7 +137,7 @@ function Nav() {
   [&::-webkit-scrollbar-thumb]:rounded-full
   [&::-webkit-scrollbar-thumb]:bg-gray-300
   dark:[&::-webkit-scrollbar-track]:bg-neutral-700
-  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 grid h-36 hover:scroll-auto rounded-b-xl overflow-y-scroll gap-3 py-2 bg-slate-900 px-5 ${!sm ? "bottom-16 rounded-t-xl" : ""}  ${searchTerm == "" || searchTerm.includes(" ") || filteredUsers.length == 0 || searchTerm.includes(data?.userName) ? "hidden" : ""} `}
+  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 grid h-36 hover:scroll-auto rounded-b-xl  gap-3 py-2 bg-slate-900 px-5 ${!sm ? "bottom-16 rounded-t-xl" : ""}  ${searchTerm == "" || searchTerm.includes(" ") || filteredUsers.length == 0 || searchTerm.includes(data?.userName) ? "hidden" : ""} `}
 
                 >
                   {filteredUsers.map(
